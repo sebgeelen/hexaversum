4a04259f52326b617a875520a69e639ab3369399
Sébastien Geelen <seb@woorank.com>
2013-11-20 18:13:10 +0100

    css & html improvement

--- js/ready.js
+++ js/ready.js
@@ -12,4 +12,20 @@ $(function() {
     console.log($(this));
   });
   /*  */
+
+  $(window).on("resize", function(e) {
+    var html    = $("html"),
+        hWidth  = html.width() - 9,
+        hHeight = html.height() - 72,
+        scrollC = $("#board-scroll");
+
+    scrollC.css({
+      "width" : hWidth + "px",
+      "height": hHeight + "px"
+    });
+
+    scrollC.scrollTop( ( 1200 - hHeight ) / 2);
+    scrollC.scrollLeft( ( 1200 - hWidth )  / 2);
+
+  }).trigger("resize");
 });
